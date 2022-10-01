fd0e2a087e8f6c13c61b3f448a73c352d2f15d9f
Ravi Shahi <ravishahi1810@gmail.com>
2022-10-01 10:42:44 +0530

    selection audio

--- js/script.js
+++ js/script.js
@@ -1,17 +1,32 @@
 // nav sound
 var totalNavLinks = document.querySelectorAll(".nav-item").length;
 for (var i = 0; i < totalNavLinks; i++) {
-    document.querySelectorAll("li.nav-item")[i].addEventListener("click", function() {
+    document.querySelectorAll("li.nav-item")[i].addEventListener("mouseover", function() {
         var audio = new Audio("audio/nave-sound.wav");
         audio.play();
-    })
+    });
+    document.querySelectorAll("li.nav-item")[i].addEventListener("click", function() {
+        var audio = new Audio("audio/sword.mp3");
+        audio.play();
+    });
 }
 
-// nave-button
+// nav-button
 
 var totalYoutubeBtn = document.querySelectorAll("button.youtube-link").length;
 for (var i = 0; i < totalYoutubeBtn; i++) {
     document.getElementsByClassName("youtube-link")[i].addEventListener("click", function() {
         open("https://youtu.be/2eQR-XSc-94");
     });
+}
+
+document.querySelector(".reg-link").addEventListener("click", function() {
+    open("./contactus.html");
+});
+
+// nav-link
+
+
+function delay(URL) {
+    setTimeout(function() { location = URL }, 750);
 }
